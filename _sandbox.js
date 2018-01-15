d1d9421d292c0d1f84b34acbf9b4f073d1b28118
pawelkarauda <zlapmniehakerze@gmail.com>
2018-01-15 20:30:16 +0100

    I prepared files for webpack.

---
+++ _sandbox.js
@@ -0,0 +1,5 @@
+require( './css/_core.sass' );
+
+window.viewability = (
+  () => require( './js/_core.js' )
+)();
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './_sandbox.js',
+  output: {
+    path: path.resolve(__dirname, './static'),
+    filename: '[name].bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+        {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+        {
+        test: /\.sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+        filename: '[name].bundle.css'
+    })
+  ]
+};
